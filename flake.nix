870b1970dcb874ea9faf358006e74b1cd0b12a16
mike7d7 <mike7d7@proton.me>
2026-05-19 21:19:21 -0600

    flake: init nix flake for easier development

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "Simulador de Banco";
+
+  # inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+
+    in
+    {
+      packages = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "osu-sim";
+            version = "0.0.1";
+            src = self;
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              gcc
+            ];
+            buildPhase = ''
+              g++ -std=c++11 main.cpp "src/standard.cpp" -o osu-sim
+            '';
+            installPhase = ''
+              mkdir -p $out/bin
+              cp osu-sim $out/bin/
+            '';
+          };
+        }
+      );
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages =
+              with pkgs;
+              [
+                gcc
+                clang-tools
+                cppcheck
+                pkg-config
+              ]
+              ++ (if system == "aarch64-darwin" then [ ] else [ lldb ]);
+            shellHook = ''
+              export SHELL=/run/current-system/sw/bin/bash
+            '';
+          };
+        }
+      );
+    };
+}
