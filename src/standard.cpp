cd23dbc0372f0d22049742e7975889728abfc5ca
mike7d7 <mike7d7@proton.me>
2026-05-21 21:18:58 -0600

    standard.cpp: code cleanup

--- src/standard.cpp
+++ src/standard.cpp
@@ -4,33 +4,6 @@
 #include <sstream>
 
 void Standard::jugar() {
-  // great = 529;
-  // ok = 3;
-  // meh = 0;
-  // miss = 1;
-  // max_combo = 546;
-
-  // great = 477;
-  // ok = 50;
-  // meh = 0;
-  // miss = 6;
-  // max_combo = 366;
-
-  // R U 4 Me - myself
-  great = 251;
-  ok = 34;
-  meh = 0;
-  miss = 3;
-  max_combo = 322;
-  star_rating = 7.1;
-
-  // Image material - Jesse Pinkman
-  // great = 2270;
-  // ok = 8;
-  // meh = 0;
-  // miss = 0;
-  // max_combo = 2900;
-
   num_notas = great + ok + meh + miss;
 
   accuracy = calc_accuracy();
@@ -44,13 +17,10 @@ int Standard::calc_puntuacion() {
   double weighted = current * 5.5 * ((double)max_combo / num_notas);
   double max_score = max * 6.85;
   int punt = weighted / max_score * 1000000 - std::pow(weighted / 10000, 2);
-  // uint32_t punt = weighted / max_score * 1000000 - std::exp(weighted /
-  // 100000) + (-0.1 * weighted + 80000);
   return punt;
 };
 
 int Standard::calc_performance() {
-  // use accuracy as exponent
   int perf = 25 * std::pow(star_rating - 1.8, 2) *
              std::pow(accuracy / 0.98, 6) *
              std::pow(max_combo / num_notas, 0.8) * std::pow(0.97, miss);
